d0ce0738f0b8081329e25f44a582c3887c72fddf
demisforever <demisgero22@gmail.com>
2023-10-23 21:35:59 -0300

    improvements in contacts

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import entrada from "../../assets/entrada.png";
+import { FaInstagram, FaWhatsapp, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { AiOutlineMail } from 'react-icons/ai';
+import {BiMap} from 'react-icons/bi'
+
 
 
 const FindUs = () => (
@@ -11,21 +15,26 @@ const FindUs = () => (
       </h1> {/*Find Us*/}
       <div className="app__wrapper-content">
         <p className="p__opensans">
-            25 de Mayo 1553, Villa Ocampo, Santa Fe
+            <BiMap /> 25 de Mayo 1553, Villa Ocampo, Santa Fe
         </p>
-        {/*<p
-          className="p__cormorant"
-          style={{ color: "#DCCA87", margin: "2rem 0" }}
-        >
-          Horarios de atención
-        </p> Opening Hours*/}
         <p className="p__opensans">Lunes a Viernes de 8 a 12hs y 16 a 20hs.</p> {/*Mon - Fri: 10:00 am - 02:00 am*/}
-        <p className="p__opensans">+549 3482 599595</p>
+        
+        <p
+          className="p__cormorant"
+          style={{  margin: "2rem 0rem 1rem 0" }}
+        >
+          Contactos
+        </p> {/*Opening Hours*/}
+        <p className="p__opensans">
+          <a style={{ textDecoration: 'none' }} href="https://wa.me/5493482599595" target="_blank">
+            <FaWhatsapp /> +549 3482 599595</a>
+        </p>        
+        <p className="p__opensans">
+          <a style={{ textDecoration: 'none' }} href='mailto:medimagenvo@gmail.com' target="_blank">
+              <AiOutlineMail /> medimagenvo@gmail.com</a>
+        </p>
+      
       </div>
-      <Link to='javascript:void(0)'
-          onClick={() => window.location = 'mailto:medimagenvo@gmail.com'}>
-          medimagenvo@gmail.com
-      </Link>
     </div>
 
     <div className="app__wrapper_img">
